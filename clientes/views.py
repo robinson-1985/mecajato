70a679bc2efb64f8be3009ccb5e3696169ee2eac
robinson <robinpaulista85@gmail.com>
2022-10-13 21:15:31 -0300

    Other commits

--- clientes/views.py
+++ clientes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Cliente, Carro
 import re
 
 def clientes(request):
     if request.method == 'GET':
-        return render(request, 'clientes.html')
+        clientes_list = Cliente.objects.all()
+        return render(request, 'clientes.html', {'clientes_list': clientes_list})
     elif request.method == 'POST':
         nome = request.POST.get('nome')
         sobrenome = request.POST.get('sobrenome')
@@ -40,3 +41,7 @@ def clientes(request):
             car.save()
 
         return HttpResponse('teste')
+
+
+def att_cliente(request):
+    return JsonResponse({'teste': 1})
