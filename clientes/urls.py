85bb862fe748510362f195befb8999ea8eeaa40a
robinson <robinpaulista85@gmail.com>
2022-11-03 13:56:11 -0300

    Add novas info no urls.py

--- clientes/urls.py
+++ clientes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.clientes, name="clientes"),
     path('atualiza_cliente/', views.att_cliente, name="atualiza_cliente"),
+    path('update_carro/<int:id>', views.update_carro, name="update_carro"),
+    path('excluir_carro/<int:id>', views.excluir_carro, name="excluir_carro")
 ]
