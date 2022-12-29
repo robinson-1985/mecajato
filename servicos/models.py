e3da2087b838f1e9fd3a43eeafe5731a7cf7d1ef
robinson <robinpaulista85@gmail.com>
2022-12-29 19:40:25 -0300

    Add new info and modified category

--- servicos/models.py
+++ servicos/models.py
@@ -1,6 +1,9 @@
+from email.policy import default
+from secrets import token_hex
 from django.db import models
 from clientes.models import Cliente
 from .choices import ChoicesCategoriaManutencao
+from datetime import datetime
 
 
 class CategoriaManutencao(models.Model):
@@ -14,10 +17,24 @@ class CategoriaManutencao(models.Model):
 class Servico(models.Model):
     titulo = models.CharField(max_length=30)
     cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
-    CategoriaManutencao = models.ManyToManyField(CategoriaManutencao)
+    categoria_manutencao = models.ManyToManyField(CategoriaManutencao)
     
     data_inicio = models.DateField(null=True)
     data_entrega = models.DateField(null=True)
     finalizado = models.BooleanField(default=False)
-    protocolo = models.CharField(max_length=32, null=True, blank=True)
+    protocolo = models.CharField(max_length=52, null=True, blank=True)
     
+    def __str__(self):
+        return self.titulo
+
+    def save(self, *args, **kwargs):
+        if not self.protocolo:
+            self.protocolo = datetime.now().strftime('%d/%m/%Y %H:%M:%S-') + token_hex(16)
+        super(Servico, self).save(*args, **kwargs)
+    
+    def preco_total(self):
+        preco_total = float(0)
+        for categoria in self.categoria_manutencao.all():
+            preco_total += float(categoria.preco)
+        
+        return preco_total
